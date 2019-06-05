d589f463b4f4db69d057425e16e39145adbe10d5
Anthony Gray <agray0232@gmail.com>
2019-06-04 20:50:33 -0400

    Completed js-dom exercise 6

--- js-dom/ex6/script.js
+++ js-dom/ex6/script.js
@@ -6,9 +6,13 @@ var counter = document.getElementById('counter');
 
 var count = 0; // This variable holds the current value of the counter!
 
-incrementButton.addEventListener('click', function(){
-
+incrementButton.addEventListener('click', function () {
+    count++;
     counter.innerHTML = count;
-    
+});
+
+decrementButton.addEventListener('click', function () {
+    count--;
+    counter.innerHTML = count;
 });
 
