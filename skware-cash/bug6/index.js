cdaae68bd26ee2b87c9db575bf5e4733f202156e
Anthony Gray <agray0232@gmail.com>
2019-06-04 20:06:10 -0400

    Fixed bug 6

--- skware-cash/bug6/index.js
+++ skware-cash/bug6/index.js
@@ -5,8 +5,7 @@
 //*******************************************************
 function renderTransactions(transactions) {
 	var finalHTML = '<div class="buffer">TRANSACTIONS</div>';
-
-	var transactionsHTML = transactions.map(function () {
+	var transactionsHTML = transactions.map(function (transaction) {
 		var transactionHTML = `
 		<div class="transaction">
 			<div class="name">${transaction.name}</div>
@@ -30,7 +29,6 @@ function renderTransactions(transactions) {
 //*******************************************************
 document.addEventListener("DOMContentLoaded", function () {
 	document.getElementById('transactions').innerHTML = renderTransactions(fullTransactionData);
-
 	document.getElementById('search-input').addEventListener('input', function (e) {
 		var searchString = e.target.value.toLowerCase();
 		var filteredData = fullTransactionData.filter(function (transaction) {
