6f4475ab9172934f0290a7bbb335e2ce206cd023
Anthony Gray <agray0232@gmail.com>
2019-06-15 10:43:55 -0400

    Added documentation to random-doggo

--- random-doggos/script.js
+++ random-doggos/script.js
@@ -1,17 +1,33 @@
+/*
+ * Define some useful variables and handles
+ */
 var doggoTypes = document.getElementById('doggo-types');
 var doggo = document.getElementsByClassName("doggo");
 var doggoButton = document.getElementsByClassName("js-dog-button");
 var doggoTypeList = document.getElementById("doggo-types");
 var breedToGet = "";
 
+/*
+ * Go to the dog.ceo API and get all of their dog breeds, then render all
+ * of the choices into the breed selector options
+ */
 var breeds = $.get("https://dog.ceo/api/breeds/list").then(renderDoggoTypes);
 
+/*
+ * Add onClick function to the doggo button. This changes the text of the button,
+ * retrieves and sets the new doggo, and resets the button
+ */
 $(doggoButton).click(function () {
     var url = getImageURL();
     $(this).text("Generating Doggo...");
     $.get(url, setNewDoggo).then(resetButton);
 });
 
+/*
+ * Sets the image URL based on whether or not there is a specific breed to request for
+ *
+ * @return url - The URL to request a dog image from
+ */
 function getImageURL() {
     var url = ""
 
@@ -19,6 +35,7 @@ function getImageURL() {
     if (breedToGet !== "") {
         url = `https://dog.ceo/api/breed/${breedToGet}/images/random`
     }
+    // Else just request a random breed
     else {
         url = `https://dog.ceo/api/breeds/image/random`
     }
@@ -26,18 +43,30 @@ function getImageURL() {
     return url;
 }
 
+/*
+ * Sets the breed to get
+ */
 function setBreed(breed) {
     breedToGet = breed.value;
 }
 
+/*
+ * Sets the source of the dog image tag
+ */
 function setNewDoggo(data) {
     $(doggo).attr("src", data.message);
 }
 
+/*
+ * Resets the doggo button text
+ */
 function resetButton() {
     $(doggoButton).text("Generate Doggo");
 }
 
+/*
+ * Render the HTML for the dog breed options and set the inner HTML
+ */
 function renderDoggoTypes(data) {
     typesHTML = "<option value=\"\">Select Breed</option>";
 
