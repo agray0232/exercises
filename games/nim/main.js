8153987fcfbf4223c92ee6644ab9a7d840d96342
Anthony Gray <agray0232@gmail.com>
2019-06-25 22:45:04 -0400

    worked on some stuff

--- games/nim/main.js
+++ games/nim/main.js
@@ -1,19 +1,43 @@
 var content = document.getElementById('content');
+var takeInput = document.getElementById('takeInput');
 
-var gameState = {
-    remainingPebbles: 3,
+var gameData = {
+    gameRunning: true,
+    refreshRate: 500,
+    gameState: "playing",
+    remainingPebbles: 16,
     activePlayer: "Player 1",
-    numToTake: 0,
+    numToTake: 3,
     maxToTake: 3,
     message: ""
 }
 
-setMessage();
-content.innerHTML = renderGame();
+setTimeout(run, gameData.refreshRate);
 
+function run() {
+    if (gameData.gameRunning) {
+        switch (gameData.gameState) {
+            case "initializing":
+                break;
+            case "playing":
+                break;
+            case "complete":
+                break;
+            default:
+                break;
+        }
+        setMessage();
+        content.innerHTML = renderGame();
+        setTimeout(run, gameData.refreshRate);
+    }
+}
+
+function setNumToTake(value) {
+    gameData.numToTake = value;
+}
 
 function setMessage() {
-    gameState.message = `It's ${gameState.activePlayer}'s turn! How many pebbles will you take?`
+    gameData.message = `It's ${gameData.activePlayer}'s turn! How many pebbles will you take?`
 }
 
 function renderGame() {
@@ -21,29 +45,13 @@ function renderGame() {
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>There are ${gameState.remainingPebbles} pebbles left</h4>
+            <h4>There are ${gameData.remainingPebbles} pebbles left</h4>
             <div class="w-50 text-center pebble-container">
                 ${renderPebbles()}
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
-                <div class="pebble"></div>
             </div>
-            <h4 class="mt-5">${gameState.message}</h4>
+            <h4 class="mt-5">${gameData.message}</h4>
             <div>
-                <select id="takeInput">
+                <select onchange="setNumToTake(value)" id="takeInput">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -55,6 +63,16 @@ function renderGame() {
 }
 
 function renderPebbles() {
-    var pebblesHTML = `<div class="pebble"></div> `.repeat(gameState.remainingPebbles);
+    var pebblesHTML = ``;
+
+    for (i = 0; i < gameData.remainingPebbles; i++) {
+        if (i < gameData.numToTake) {
+            pebblesHTML = pebblesHTML.concat(`<div class="pebble highlight"></div> `);
+        }
+        else {
+            pebblesHTML = pebblesHTML.concat(`<div class="pebble"></div> `);
+        }
+    }
+
     return pebblesHTML;
 }
