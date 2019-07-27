1ad78617832ef849301b09bc359d26329d79840d
Anthony Gray <agray0232@gmail.com>
2019-07-27 13:00:01 -0400

    Initial sql imdb commit

--- nodejs/pets-api/server.js
+++ nodejs/pets-api/server.js
@@ -37,10 +37,43 @@ var owners = [
 
 
 // GET /api/owners
+app.get('/api/owners', (req, res, next) => {
+    res.json(owners);
+})
 
 // GET /api/owners/:id
+app.get('/api/owners/:id', (req, res, next) => {
+    var resOwner = owners.find(owner => owner.id == req.params.id);
+
+    if (resOwner) {
+        res.json(resOwner);
+    }
+})
 
 // POST /api/owners
+app.post('/api/owners', (req, res, next) => {
+    var newOwner = {};
+    console.log(req.params);
+    newOwner.name = req.params.name;
+    newOwner.id = getNextOwnerId();
+
+    var pets = [];
+    var petCounter = 1;
+    req.params.pets.forEach(pet => {
+        pets.push(
+            {
+                id: petCounter,
+                name: pet.name,
+                type: pet.type
+            })
+        petCounter++;
+    })
+    newOwner.pets = pets;
+
+    owners.push(newOwner);
+
+    res.json(newOwner);
+})
 
 // PUT /api/owners/:id
 
@@ -57,6 +90,18 @@ var owners = [
 // DELETE /api/owners/:id/pets/:petId
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Pets API is now listening on port 3000...');
 })
+
+function getNextOwnerId() {
+    var highestId = 0;
+
+    owners.forEach(owner => {
+        if (owner.id > highestId) {
+            highestId = owner.id;
+        }
+    })
+
+    return highestId++;
+}
