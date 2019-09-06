ae981d7858672fe307fd517947ecc42b66f30fe2
Anthony Gray <agray0232@gmail.com>
2019-09-05 21:02:18 -0400

    Completed redux intro exercises

--- redux/redux-simple-counter/src/index.js
+++ redux/redux-simple-counter/src/index.js
@@ -9,4 +9,20 @@ subscribe(() => render(getState()))
 
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const increment5Button = document.getElementById('increment-5');
+const decrement5Button = document.getElementById('decrement-5');
+const colorPicker = document.getElementById('colorPicker');
+const userInput = document.getElementById('userInput');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
+increment5Button.addEventListener('click', e => dispatch({ type: "INCREMENT5" }));
+decrement5Button.addEventListener('click', e => dispatch({ type: "DECREMENT5" }));
+colorPicker.addEventListener('change', e => dispatch({
+    type: "COLOR_CHANGE",
+    color: e.target.value
+}));
+userInput.addEventListener('input', e => dispatch({
+    type: "USER_SET_NUMBER",
+    userNumber: e.target.value
+}));
--- redux/redux-simple-counter/src/reducer.js
+++ redux/redux-simple-counter/src/reducer.js
@@ -1,13 +1,42 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: "black"
 }
 
 const reducer = (state = initialState, action) => {
     let { type } = action;
-    if (type === "INCREMENT") {
-        return {
-            value: state.value + 1
-        }
+
+    switch (type) {
+        case "INCREMENT":
+            return {
+                color: state.color,
+                value: Number(state.value) + 1
+            }
+        case "DECREMENT":
+            return {
+                color: state.color,
+                value: Number(state.value) - 1
+            }
+        case "INCREMENT5":
+            return {
+                color: state.color,
+                value: Number(state.value) + 5
+            }
+        case "DECREMENT5":
+            return {
+                color: state.color,
+                value: Number(state.value) - 5
+            }
+        case "COLOR_CHANGE":
+            return {
+                value: state.value,
+                color: action.color
+            }
+        case "USER_SET_NUMBER":
+            return {
+                value: document.getElementById('userInput').value
+            }
+        default:
+            return state;
     }
-    return state
 }
