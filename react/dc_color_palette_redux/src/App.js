edf4190df30061ded9e150cdeebbfbde9fee5127
Anthony Gray <agray0232@gmail.com>
2019-10-01 23:21:37 -0400

    Added react redux exercise

---
+++ react/dc_color_palette_redux/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ColorPaletteContainer from './containers/ColorPaletteContainer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPaletteContainer />
+    </div>
+  );
+}
+
+export default App;
